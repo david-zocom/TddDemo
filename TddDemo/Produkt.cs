b4b9900484f7a5f5940c95690bd2463b9e2b1ac9
David Andersson <david.andersson@zocom.se>
2019-12-02 16:11:35 +0100

    Exercise 1

---
+++ TddDemo/Produkt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TddDemo
+{
+	/*
+	 1 Skapa en klass som beskriver något man kan sälja i en affär. Den ska ha namnet Produkt och egenskaperna namn och prisExMoms (double). Skapa metoderna PrisExklusiveMoms och PrisInklusiveMoms. Den första ska returnera priset direkt, den andra ska returnera priset inklusive moms. Om något kostar 100 kr utan moms så kostart det 25% mer med moms, dvs 125 kr.
+	Skriv testfall som testar att funktionerna returnerar rätt värde. Ett testfall per funktion.
+
+		 */
+	public class Produkt
+	{
+		private string namn;
+		private double prisExMoms;
+		public Produkt(string namn, double prisExMoms)
+		{
+			this.namn = namn;
+			this.prisExMoms = prisExMoms;
+		}
+
+		public double PrisInklusiveMoms()
+		{
+			return this.prisExMoms * 1.25;
+		}
+
+		public double PrisExklusiveMoms()
+		{
+			return prisExMoms;
+		}
+	}
+}
---
+++ TestTddDemo/TestProdukt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TddDemo;
+using Xunit;
+
+namespace TestTddDemo
+{
+	public class TestProdukt
+	{
+		// [Theory] can run several different values
+		[Fact]
+		public void PrisExklusiveMoms_SkaReturneraPrisetDirekt()
+		{
+			// arrange
+			double expected = 999.5;
+			Produkt produkt = new Produkt("dator", expected);
+
+			// act
+			double actual = produkt.PrisExklusiveMoms();
+
+			// assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void PrisInklusiveMoms_SkaReturneraPrisetPlusMoms()
+		{
+			// arrange
+			double price = 225;
+			double expected = price * 1.25;
+			Produkt produkt = new Produkt("wrench", price);
+
+			// act
+			double actual = produkt.PrisInklusiveMoms();
+
+			// assert
+			Assert.Equal(expected, actual);
+		}
+	}
+}
